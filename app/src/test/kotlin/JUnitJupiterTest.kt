b1171ef4421f67373b83121668d6c2dec39eed60
Daniel Alves Mertins <daniel.mertins@dmertinss-MacBook-Pro.local>
2025-03-19 10:42:34 -0300

    Add JUnit test sample

---
+++ app/src/test/kotlin/JUnitJupiterTest.kt
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class JUnitJupiterTests {
+    @Test fun `test something`() {
+        assertEquals(2, 1 + 1)
+    }
+}
